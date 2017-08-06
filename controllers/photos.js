c27c9b19e679afc0f276c670868b89b270457a37
Mike Skelton <mskelton19@Mikes-MBP.home>
2017-08-06 12:54:00 -0400

    added photos functionality

--- controllers/photos.js
+++ controllers/photos.js
@@ -3,17 +3,35 @@ const router = express.Router();
 const Photo = require('../models/photos.js');
 
 router.get('/photos', (req, res) => {
-  res.render('photos/index.ejs');
+  Photo.find ({}, (error, allPhotos) => {
+    res.render('photos/index.ejs', {
+      photo: allPhotos
+    });
+  })
+})
+
+router.post('/photos', (req, res) => {
+  Photo.create(req.body, (error, createPhoto) => {
+    res.redirect('/photos')
+  });
+});
+
+router.get('/photos/new', (req, res) => {
+  res.render('photos/new.ejs');
 })
 
 
 router.get('photos/:id', (req, res) => {
-  res.render('photos/show.ejs')
+  Photo.findById(req.params.id, (error, foundPhoto) => {
+    res.render('photos/show.ejs', {
+      photo: foundPhoto
+    });
+  });
 })
 
 
 router.get('/photos/:id', (req, res) => {
-  User.findById(req.params.id, (error, User) => {
+  Photo.findById(req.params.id, (error, Photo) => {
     res.render('photos/show.ejs', {
       photo: Photo
     })
